9ecfbe95bd6ac1f875381f3a0e277f4ee094aff5
Daehyun Kim <bigdarkgold@gmail.com>
2025-01-19 22:56:07 +0900

    [Bronze V] Title: X보다 작은 수, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -5,9 +5,5 @@ N, X = map(int, input().split())
 A = list(map(int, input().split()))
 
 for i in range(N):
-    # X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다.
     if A[i] < X:
         print(A[i], end=" ")
-
-# X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다.
-# X보다 작은 수는 적어도 하나 존재한다.
